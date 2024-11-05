680384020847429b96a43a33c3c669d0af5a591c
Mr Damn Coder <luantranphu901@gmail.com>
2024-11-05 16:36:46 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode(( SCREEN_WIDTH, SCREEN_HEIGHT ))
+pygame.display.set_caption("Kingdom Fighter")
+
+
+run = True
+while run:
+    
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+pygame.quit()
